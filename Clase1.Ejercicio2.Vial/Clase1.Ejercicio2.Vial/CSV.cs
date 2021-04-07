6a6d9fb74f2311a8f61f71cbf5461142d778f9d0
marcosdlopez <marcos.daniellopez2020@gmail.com>
2021-04-07 19:53:31 -0300

    Ejercicio2

---
+++ Clase1.Ejercicio2.Vial/Clase1.Ejercicio2.Vial/CSV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    class CSV
+    {
+        private string[] line;
+
+        public CSV() { }
+
+        public string[] Archivo()
+        {
+            line = File.ReadAllLines( @"C: \Users\Mark\Documents\GitHub\-PCyP.LopezMarcosDaniel.2021\Clase1.Ejercicio2.Vial\Clase1.Ejercicio2.Vial");
+
+            return line;
+        }
+
+
+        public void Ver()
+        {
+
+
+            foreach (var i in Archivo())
+            {
+                var linea = i.Split(',');
+
+                Console.WriteLine($"{linea[0]}{"  "}{linea[1]}{"  "}{linea[2]}{"  "}{linea[3]}{"  "}{linea[4]}");
+            }
+
+        }
+
+    }
+
+
+
+}
---
+++ Clase1.Ejercicio2.Vial/Clase1.Ejercicio2.Vial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CSV a = new CSV();
+
+            a.Ver();
+        }
+    }
+}
