b4392e1ec1b860784bbdd5674670e53716ef313d
marcosdlopez <marcos.daniellopez2020@gmail.com>
2021-04-07 21:44:19 -0300

    Ejercicio3

---
+++ Clase3.Ejercicio1.Manejo_Diccionario/Clase3.Ejercicio1.Manejo_Diccionario/ManejoDic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio1.Manejo_Diccionario
+{
+    class ManejoDic
+    {
+        //CREAMOS EL DICCIONARIO 
+
+        Dictionary<string, string> values = new Dictionary<string, string>();
+
+        public ManejoDic()
+        {
+
+            values.Add("Juan", "55423412");
+            values.Add("Ernesto", "56985623");
+            values.Add("Mariana", "54787451");
+        }
+
+        public string EncontrarLlave(string Nombres)
+        {
+            string suValor = "";
+
+            if (values.TryGetValue(Nombres, out suValor))
+            {
+                return suValor;
+            }
+            else
+            {
+                return "NO CONTIENE LLAVE";
+            }
+        }
+
+        public void LlaveBusqueda(String Nombres)
+        {
+            if (values.ContainsKey(Nombres))
+            {
+                Console.WriteLine(values[Nombres]);
+            }
+            else
+            {
+                Console.WriteLine("No existe llave");
+            }
+        }
+
+        public void ValoresAlt(string indice, string suValor)
+        {
+            if (values.ContainsKey(indice))
+            {
+                values[indice] = suValor;
+                Console.WriteLine("Telefono de {0} es {1}", suValor, indice);
+            }
+            else
+            {
+                Console.WriteLine("No Existe Indice");
+
+
+
+
+            }
+        }
+        public void Mostrar()
+        {
+
+            foreach (var recorrido in values)
+            {
+
+                Console.WriteLine(recorrido.Key + "," + recorrido.Value);
+
+
+            }
+
+        }
+    }
+}
---
+++ Clase3.Ejercicio1.Manejo_Diccionario/Clase3.Ejercicio1.Manejo_Diccionario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio1.Manejo_Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            ManejoDic Manejo = new ManejoDic();
+
+            Manejo.ValoresAlt("Mariana", "58251425");
+
+            Manejo.EncontrarLlave("Juan");
+
+            Console.WriteLine(Manejo.EncontrarLlave("pedro"));
+
+            Manejo.Mostrar();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+    
+ 
